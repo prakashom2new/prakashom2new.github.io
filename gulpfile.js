94159f46ad00e46cd12ab495d075f2fa998ae931
Omprakash Arumugam <omprakash_a@outlook.com>
2017-02-10 04:11:52 +0530

    gulp setup - tasks: fileinclude, watch, plumber

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,44 @@
 var gulp = require ("gulp"),
     watch = require ("gulp-watch"),
-    del = require ("del"),
     cssMinify = require ("gulp-csso"),
     imgMin = require ("gulp-imagemin"),
-    fldr = { src: "src/", build: "build/" };
+    fileIncluder = require ("gulp-file-include"),
+    rename = require ("gulp-rename"),
+    plumber = require ("gulp-plumber"),
+    webserver = require('gulp-webserver');;
 
-gulp.task ("css", function () {
+gulp.task ("fileInclude", function () {
+    return gulp.src ("/templates/*.tpl.html")
+            .pipe (plumber ())    
+            .pipe (fileIncluder ())
+            .pipe (rename ({extname: ""}))
+            .pipe (rename ({extname: ".html"}))
+            .pipe (gulp.dest ("./"));
+});
+gulp.task ("fileIncludeWorks", function () {
+    return gulp.src ("./works_pages/templates/*.tpl.html")
+            .pipe (plumber ())    
+            .pipe (fileIncluder ())
+            .pipe (rename ({extname: ""}))
+            .pipe (rename ({extname: ".html"}))
+            .pipe (gulp.dest ("./works_pages"));
+});
+
+gulp.task('webserver', function() {
+  gulp.src('./')
+    .pipe(webserver({
+      livereload: true,
+      directoryListing: true,
+      open: true
+    }));
+
+    gulp.watch ("./templates/*.tpl.html", ["fileInclude"]);
+    gulp.watch ("./works_pages/templates/*.tpl.html", ["fileIncludeWorks"]);
+    
+});
+
+
+/*gulp.task ("css", function () {
     return gulp.src (fldr.src + "/css/*.css")
     .pipe (cssMinify ())
     .pipe (gulp.dest (fldr.build + "/css"));
@@ -17,15 +50,10 @@ gulp.task ("image", function () {
     .pipe (gulp.dest (fldr.build + "/img"));
 });
 
-gulp.task ("watch", function () {
-    gulp.watch (fldr.src + "/css/*.css", ["css"]);
-    gulp.watch (fldr.src + "/img/*", ["image"]);
-});
-
-/*gulp.task('cleanBuild', function () {
+gulp.task('cleanBuild', function () {
   return del([
     fldr.build + "/**"
   ]);
 });*/
 
-gulp.task ("default", ["css", "image", "watch"]);
+gulp.task ("default", ["webserver", "fileInclude", "fileIncludeWorks"]);
